29219a813850e406c426978fba5aff729251aca4
Hiago Prates <hiagohps360@gmail.com>
2024-06-03 09:37:35 -0300

    feat: create progress component

---
+++ src/components/Progress/index.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react'
+import CircularProgress from '@mui/material/CircularProgress'
+import { Container } from './styles'
+
+const Progress = () => {
+  return (
+    <Container>
+      <CircularProgress />
+    </Container>
+  )
+}
+
+export default Progress
---
+++ src/components/Progress/styles.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+
+  background-color: #000;
+  opacity: 0.5;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
